ffb7349b68aecbd12459ec8a180958f49c787ced
Sjens <scjensen813@live.com>
2021-03-26 14:10:51 -0700

    deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDdas_pBef6akaOlBTzcW3NNK53-hjImI8",
+    authDomain: "nutri-plant.firebaseapp.com",
+    projectId: "nutri-plant",
+    storageBucket: "nutri-plant.appspot.com",
+    messagingSenderId: "388523793127",
+    appId: "1:388523793127:web:351b2d7ecaaab6a76a6042",
+    measurementId: "G-QPX6X2LPPZ"
+  }
 };
